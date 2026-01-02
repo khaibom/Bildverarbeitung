96d2ebaa93be3c7dca9bffa0f6353047bd058286
khaibom <tdkhai.work@gmail.com>
2026-01-02 11:14:06 +0100

    Aufgabe 39

--- P3/ConsoleApplication_Task3/ConsoleApplication_Task3.cpp
+++ P3/ConsoleApplication_Task3/ConsoleApplication_Task3.cpp
@@ -112,6 +112,33 @@ cv::Mat getResultImage(cv::Mat testImage, cv::Mat firstProcessedTestImage, cv::M
 }
 // <<< Aufgabe 29
 
+// Aufgabe 33
+int g_rotationAngleDeg = 0;
+
+// >>> Aufgabe 34
+static void onRotationAngleDegTrackbar(int pos, void*){
+    g_rotationAngleDeg = pos;
+    std::cout << "g_rotationAngleDeg = " << g_rotationAngleDeg << std::endl;
+}
+// <<< Aufgabe 34
+
+// >>> Aufgabe 36
+cv::Mat getMask(cv::Mat axisAlignedMask, int rotationAngleDeg){
+    cv::Mat rotatedMask;
+    const cv::Point2f center(axisAlignedMask.cols / 2.0f, axisAlignedMask.rows / 2.0f);
+    cv::Mat rotationMatrix = cv::getRotationMatrix2D(center, rotationAngleDeg, 1.0);
+    cv::warpAffine(
+        axisAlignedMask,
+        rotatedMask,
+        rotationMatrix,
+        axisAlignedMask.size(),
+        cv::INTER_NEAREST,
+        cv::BORDER_CONSTANT,
+        cv::Scalar(0)
+    );
+    return rotatedMask;
+}// <<< Aufgabe 36
+
 int main()
 {
     cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_SILENT);
@@ -138,6 +165,10 @@ int main()
     cv::imshow("axisAlignedMask", axisAlignedMask);
     // <<< Aufgabe 28
 
+    // >>> Aufgabe 35
+    cv::createTrackbar("rotation angle", resultImageWindowTitle, nullptr, 90, onRotationAngleDegTrackbar);
+    // <<< Aufgabe 35
+
     // >>> Aufgabe 19, 20, 21
     while (FindWindow(NULL, resultImageWindowTitleW.c_str())){
         const int sideLength = g_maxWavelength * 2 + 1;
@@ -161,6 +192,13 @@ int main()
         cv::Mat partitionedResultImage = getResultImage(resultImage, firstProcessedTestImage, secondProcessedTestImage, axisAlignedMask);
         cv::imshow(resultImageWindowTitle, partitionedResultImage);
         // <<< Aufgabe 30, 31
+
+        // Aufgabe 37
+        cv::Mat mask = getMask(axisAlignedMask, g_rotationAngleDeg);
+
+        // Aufgabe 38
+        cv::Mat partitionedResultImage = getResultImage(resultImage, firstProcessedTestImage, secondProcessedTestImage, mask);
+        cv::imshow(resultImageWindowTitle, partitionedResultImage);
 
         int key = cv::waitKey(30);
         if (key == 27) break;
